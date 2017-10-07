c23d0cd25cb3dd707684bc41444bf685e44189f7
dorukgezici <dorukgezici@gmail.com>
2017-10-07 16:39:47 +0300

    Cask created.

---
+++ Casks/popcorn-time.rb
@@ -0,0 +1,10 @@
+cask 'popcorn-time' do
+  version '0.3.10'
+  sha256 '62002814ddac7587e447d0f8e7db5d7359ccca77893b79223f7b1f5c7d8992bf'
+
+  url 'https://get.popcorntime.sh/build/Popcorn-Time-0.3.10-Mac.zip'
+  name 'Popcorn Time'
+  homepage 'https://get.popcorntime.sh'
+
+  app 'Popcorn-Time.app'
+end
